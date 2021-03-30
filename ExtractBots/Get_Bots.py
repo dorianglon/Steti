fa5f854186cdf6c6f31e0d2af1beda4a834a8e7d
Dorian <glondorian2001@gmail.com>
2021-03-29 20:41:43 -0700

    makes list of bots

---
+++ ExtractBots/Get_Bots.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+
+
+def get_bots(driver, url, destination):
+
+    not_bots = ['Rank', 'Bot Name', 'Score', 'Good Bot Votes', 'Bad Bot Votes', 'Comment Karma', 'Link Karma']
+    page = 1
+
+    while page <= 144:
+
+        this_url = url + str(page)
+        driver.get(this_url)
+
+        table = driver.find_element_by_xpath("//table[@class='table']")
+        links = table.find_elements_by_tag_name('a')
+        for link in links:
+            bot = link.text
+            if bot not in not_bots:
+                with open(destination, 'a+') as f:
+                    f.write(bot + '\n')
+        page += 1
+
+
+def main():
+    destination = '/Users/dorianglon/Desktop/BPG_limited/bots.txt'
+    url = 'https://botrank.pastimes.eu/?sort=rank&page='
+    driver = webdriver.Safari()
+    get_bots(driver, url, destination)
+
+
+if __name__ == '__main__':
+    main()
